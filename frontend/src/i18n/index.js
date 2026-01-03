dbe0d1a34219ee285fac4a23eb39d42f90f297ce
Morrie <morriedog@hotmail.com>
2026-01-04 02:37:41 +0800

    Add zh language

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import en from './locales/en.json'
 import ja from './locales/ja.json'
+import zh from './locales/zh.json'
 
 const savedLocale = localStorage.getItem('chatmate_locale') || 'en'
 
@@ -8,7 +9,7 @@ const i18n = createI18n({
   legacy: false,
   locale: savedLocale,
   fallbackLocale: 'en',
-  messages: { en, ja }
+  messages: { en, ja, zh }
 })
 
 export default i18n
